d14285ba4d515c59af964080b8470895cc8718e5
Kyung Jae Lee <dlrudwo1269@gmail.com>
2023-04-17 01:12:51 +0900

    Make script python native to be cross-platform

---
+++ examples/bm25_en_caption/convert_jsonl_module.py
@@ -0,0 +1,140 @@
+from pathlib import Path
+from datasets import load_dataset, concatenate_datasets
+from textacy import preprocessing
+
+
+def get_pipeline():
+    preproc = preprocessing.make_pipeline(
+        preprocessing.remove.html_tags,
+        preprocessing.normalize.unicode,
+        preprocessing.normalize.whitespace,
+        preprocessing.normalize.bullet_points,
+        preprocessing.normalize.quotation_marks,
+        preprocessing.replace.hashtags,
+        preprocessing.remove.brackets,
+        preprocessing.replace.urls
+    )
+    return preproc
+
+
+def convert_flatten_image(example, id_col):
+    result = dict()
+    result['id'] = example[id_col]
+    pipe = get_pipeline()
+    temp = []
+    # filter en
+    en_pos = []
+    for _, lang_id in enumerate(example['language']):
+        if lang_id == 'en':
+            en_pos.append(True)
+        else:
+            en_pos.append(False)
+
+    for key, val in example.items():
+        if key == id_col:
+            continue
+        if key == 'language':
+            continue
+
+        content = []
+        if isinstance(val, list):
+            for item, valid in zip(val, en_pos):
+                if valid:
+                    clean = pipe(item)
+                    content.append(clean)
+        else:
+            clean = pipe(val)
+            content.append(clean)
+        text = ' '.join(content)
+        result[key] = text
+        temp.append(text)
+    result['contents'] = ' '.join(temp)
+    return result
+
+
+def convert_flatten_text(example, id_col):
+    result = dict()
+    result['id'] = example[id_col]
+    result['contents'] = ''
+    pipe = get_pipeline()
+    temp = []
+    for key, val in example.items():
+        if key == id_col:
+            continue
+
+        content = []
+        if isinstance(val, list):
+            for item in val:
+                clean = pipe(item)
+                content.append(clean)
+        else:
+            clean = pipe(val)
+            content.append(clean)
+        text = ' '.join(content)
+        result[key] = text
+        temp.append(text)
+    result['contents'] = ' '.join(temp)
+    return result
+
+
+def limit_clause(example, limit=1024):
+    # truncate the number of 'words' by space
+    clauses = example['contents'].split()
+    return {'contents': ' '.join(clauses[:limit])}
+
+
+def maybe_shard_to_save(dataset, output_path, num_lines=1000000):
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    if len(dataset) > num_lines:
+        n_shards = len(dataset) // num_lines + 1
+        for idx in range(n_shards):
+            subset = dataset.shard(num_shards=n_shards, index=idx)
+            output_file = Path(output_path.parent, output_path.stem + f'.part-{idx:02d}.jsonl')
+            subset.to_json(str(output_file))
+    else:
+        dataset.to_json(str(output_path))
+
+
+def encode(split, encode_field, qrels_ds, image_ds, text_ds, output_dir):
+    if split == 'other':
+        qrels = load_dataset(qrels_ds)
+        qrels = concatenate_datasets(
+            [qrels[qrels_split] for qrels_split in ['train', 'validation', 'test']]
+        )
+    else:
+        qrels = load_dataset(qrels_ds, split=split)
+
+    if encode_field == 'image_caption':
+        valid_images = set(qrels.unique('image_id'))
+
+        images = load_dataset(image_ds, split='train')
+        # remove images for faster processing
+        images = images.remove_columns(['image', 'image_url'])
+        old_col = images.column_names
+
+        if split == 'other':
+            images = images.filter(lambda example: example['image_id'] not in valid_images, num_proc=32)
+        else:
+            images = images.filter(lambda example: example['image_id'] in valid_images, num_proc=32)
+
+        images = images.map(convert_flatten_image, fn_kwargs={'id_col': 'image_id'}, num_proc=16)
+        images = images.map(limit_clause, num_proc=16)
+        images = images.remove_columns(old_col)
+        maybe_shard_to_save(images, output_dir / 'image-collection' / f'{split}.image-caption.jsonl')
+
+    elif encode_field == 'text':
+        valid_texts = set(qrels.unique('text_id'))
+
+        texts = load_dataset(text_ds, split='train')
+        texts = texts.remove_columns(['media', 'category', 'source_id', 'page_url'])
+        old_col = texts.column_names
+
+        if split == 'other':
+            texts = texts.filter(lambda example: bool(example['text_id'] not in valid_texts), num_proc=32)
+        else:
+            texts = texts.filter(lambda example: bool(example['text_id'] in valid_texts), num_proc=32)
+
+        texts = texts.map(convert_flatten_text, fn_kwargs={'id_col': 'text_id'}, num_proc=16)
+        texts = texts.map(limit_clause, num_proc=16)
+        texts = texts.remove_columns(old_col)
+        maybe_shard_to_save(texts, output_dir / 'text-collection' / f'{split}.text.jsonl')
--- examples/bm25_en_caption/run_bm25_baseline.py
+++ examples/bm25_en_caption/run_bm25_baseline.py
@@ -13,61 +13,190 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import os
+from pathlib import Path
+import argparse
 
 from datasets import load_dataset
+from pyserini.setup import configure_classpath
+
+from convert_jsonl_module import encode
 
 
 SPLITS = ["train", "validation", "test", "other"]
 SETTINGS = ["small", "base", "large"]
+ENCODING_FIELDS = ["text", "image_caption"]
 
 
-def prep_qrels(split):
-    qrel_ds = load_dataset('TREC-AToMiC/AToMiC-Qrels-v0.2', split=split)
+def get_args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pyserini_path", required=True, type=str, help="Path to the Pyserini installation")
+    parser.add_argument("--anserini_fatjar_path", required=True, type=str, help="Path to the Anserini fatjar")
+    parser.add_argument("--images", default="TREC-AToMiC/AToMiC-Images-v0.2", type=str)
+    parser.add_argument("--texts",  default="TREC-AToMiC/AToMiC-Texts-v0.2", type=str)
+    parser.add_argument("--qrels",  default="TREC-AToMiC/AToMiC-Qrels-v0.2", type=str)
+    return parser
+
+
+def prep_qrels(qrels_ds, split, pyserini_path):
+    qrels_dir = pyserini_path / "collections" / "atomic"
+    qrels_dir.mkdir(exist_ok=True)
+
+    qrel_ds = load_dataset(qrels_ds, split=split)
     qrel_ds.to_csv(
-        f"{split}.qrels.t2i.projected.trec", header=None, sep=" ", index=False
+        qrels_dir / f"{split}.qrels.t2i.projected.trec", header=None, sep=" ", index=False
     )
     qrel_ds.to_csv(
-        f"{split}.qrels.t2i.projected.trec",
+        qrels_dir / f"{split}.qrels.i2t.projected.trec",
         columns=["image_id", "Q0", "text_id", "rel"], header=None, sep=" ", index=False
     )
 
 
-def prep_encodings(split, encode_field):
-    cmd = f"python convert_jsonl.py --split {split} --encode_field {encode_field}"
-    os.system(cmd)
+def anserini_index(indexing_args):
+    # this import is deliberately placed here (has to be imported AFTER configure_classpath is run)
+    from jnius import autoclass
+    IndexCollection = autoclass("io.anserini.index.IndexCollection")
+    IndexCollection.main(indexing_args)
 
 
-def create_index(setting):
-    cmd = f"./index_anserini.sh index {setting}"
-    os.system(cmd)
+def create_index(setting, split, pyserini_path):
+    atomic_collections_path = pyserini_path / "collections" / "atomic"
+    if setting == "small" and not split:
+        raise Exception('Please provide the split when setting="small"')
+    postfix = "." + split if setting == "small" else ""
+
+    # create a directory that contains the documents we want to index
+    # NOTE: should we consider cleaning up this directory once we are done indexing?
+    text_dir = atomic_collections_path / f"text-collection.{setting}{postfix}"
+    text_dir.mkdir(exist_ok=True)
+
+    image_dir = atomic_collections_path / f"image-collection.{setting}{postfix}"
+    image_dir.mkdir(exist_ok=True)
+
+    if setting == "small":
+        text_jsonl_paths = list((atomic_collections_path / "text-collection").glob(f"{split}*.jsonl"))
+        image_jsonl_paths = list((atomic_collections_path / "image-collection").glob(f"{split}*.jsonl"))
+    elif setting == "base":
+        text_jsonl_paths = []
+        image_jsonl_paths = []
+        for split in ["train", "validation", "test"]:
+            text_jsonl_paths.extend(list((atomic_collections_path / "text-collection").glob(f"{split}*.jsonl")))
+            image_jsonl_paths.extend(list((atomic_collections_path / "image-collection").glob(f"{split}*.jsonl")))
+    elif setting == "large":
+        text_jsonl_paths = list((atomic_collections_path / "text-collection").glob("*.jsonl"))
+        image_jsonl_paths = list((atomic_collections_path / "image-collection").glob("*.jsonl"))
+
+    for p in text_jsonl_paths:
+        (text_dir / p.name).symlink_to(p, target_is_directory=False)
+
+    for p in image_jsonl_paths:
+        (image_dir / p.name).symlink_to(p, target_is_directory=False)
+
+    print(f"Indexing text: {setting}{postfix}")
+    text_indexing_args = [
+        "-input", str(text_dir.resolve()),
+        "-collection", "JsonCollection",
+        "-index", f"indexes/lucene-index.atomic.text.flat.{setting}{postfix}",
+        "-generator", "DefaultLuceneDocumentGenerator",
+        "-threads", "8", "-storePositions", "-storeDocvectors", "-storeRaw",
+    ]
+    anserini_index(text_indexing_args)
+
+    print(f"Indexing image: {setting}{postfix}")
+    image_indexing_args = [
+        "-input", str(image_dir.resolve()),
+        "-collection", "JsonCollection",
+        "-index", f"indexes/lucene-index.atomic.image.flat.{setting}{postfix}",
+        "-generator", "DefaultLuceneDocumentGenerator",
+        "-threads", "8", "-storePositions", "-storeDocvectors", "-storeRaw",
+    ]
+    anserini_index(image_indexing_args)
 
 
-def search_anserini(split, setting):
-    cmd = f"./search_anserini.sh {split} {setting}"
-    os.system(cmd)
+def anserini_search(search_args):
+    # this import is deliberately placed here (has to be imported AFTER configure_classpath is run)
+    from jnius import autoclass
+    SearchCollection = autoclass("io.anserini.search.SearchCollection")
+    SearchCollection.main(search_args)
 
 
-def main():
-    # TODO: maybe can add a command line argument to skip qrels, encoding, and indexing
-    #       (or altogether separate them out in a separate command)
+def search_anserini(split, setting, pyserini_path):
+    postfix = "." + split if setting == "small" else ""
+
+    indexes = pyserini_path / "indexes"
+    text_index_dir = indexes / f"lucene-index.atomic.text.flat.{setting}{postfix}"
+    image_index_dir = indexes / f"lucene-index.atomic.image.flat.{setting}{postfix}"
+
+    runs = pyserini_path / "runs"
+    runs.mkdir(exist_ok=True)
+    t2i_run_dir = runs / f"run.{split}.bm25-anserini-default.t2i.{setting}.trec"
+    i2t_run_dir = runs / f"run.{split}.bm25-anserini-default.i2t.{setting}.trec"
+
+    i2t_search_args = [
+        "-index", str(text_index_dir.resolve()),
+        "-topics", str((pyserini_path / "collections" / "atomic" / f"image-collection.{setting}{postfix}/{split}.image-caption.jsonl").resolve()),
+        "-topicreader", "JsonString",
+        "-topicfield", "contents",
+        "-output", str(i2t_run_dir.resolve()),
+        "-bm25", "-hits", "1000", "-parallelism", "64", "-threads", "64"
+    ]
+    anserini_search(i2t_search_args)
+
+    t2i_search_args = [
+        "-index", str(image_index_dir.resolve()),
+        "-topics", str((pyserini_path / "collections" / "atomic" / f"text-collection.{setting}{postfix}/{split}.text.jsonl").resolve()),
+        "-topicreader", "JsonString",
+        "-topicfield", "contents",
+        "-output", str(t2i_run_dir.resolve()),
+        "-bm25", "-hits", "1000", "-parallelism", "64", "-threads", "64"
+    ]
+    anserini_search(t2i_search_args)
+
+    """
+    TODO: seprate out this logic for evaluating in a different script
+    echo "==== Text2Image ===="
+    t2i_qrel="${QREL_DIR}/${SPLIT}.qrels.t2i.projected.trec"
+    $PATH_TREC_EVAL -c -m recip_rank -M 10 ${t2i_qrel} ${t2i_run}
+    $PATH_TREC_EVAL -c -m recall.10,1000 ${t2i_qrel} ${t2i_run} 
+
+    echo "==== Image2Text ===="
+    i2t_qrel="${QREL_DIR}/${SPLIT}.qrels.i2t.projected.trec"
+    $PATH_TREC_EVAL -c -m recip_rank -M 10 ${i2t_qrel} ${i2t_run}
+    $PATH_TREC_EVAL -c -m recall.10,1000 ${i2t_qrel} ${i2t_run}
+    """
+
+
+def main(args):
+    configure_classpath(args.anserini_fatjar_path)
+    pyserini_path = Path(args.pyserini_path)
+
     for split in SPLITS:
-        prep_qrels(split)
+        if split == "other":
+            continue
+        print(f"RUN PREP QRELS, split: {split}")
+        prep_qrels(args.qrels, split, pyserini_path)
 
     for split in SPLITS:
-        for encoding_field in ["text", "image_caption"]:
-            prep_encodings(split, encoding_field)
+        for encoding_field in ENCODING_FIELDS:
+            print(f"RUN ENCODE, split: {split}, encoding_field: {encoding_field}")
+            encode(split, encoding_field, args.qrels, args.images, args.texts, pyserini_path / "collections" / "atomic")
 
     for setting in SETTINGS:
-        create_index(setting)
+        print(f"RUN CREATE INDEX, setting: {setting}")
+        if setting == "small":
+            create_index(setting, "validation", pyserini_path)
+            create_index(setting, "test", pyserini_path)
+        else:
+            create_index(setting, None, pyserini_path)
 
     for setting in SETTINGS:
-        print(f"Evaluate on setting {setting} (validation)")
-        search_anserini("validation", setting)
-
-    # NOTE: should we do cleanup? e.g. remove any files/ symlinks we created while evaluating
-    # NOTE: also need to take care of setting the environment variables in the shell scripts
+        print(f"RUN SEARCH, setting: {setting}")
+        search_anserini("validation", setting, pyserini_path)
 
 
 if __name__ == "__main__":
-    main()
+    # TODO: maybe can add a command line argument to skip qrels, encoding, indexing
+    # Sample run command:
+    # python run_bm25_baseline.py --pyserini_path "/home/kj25lee/scratch/pyserini" --anserini_fatjar_path "/home/kj25lee/scratch/pyserini/resources/jars"
+    parser = get_args_parser()
+    args = parser.parse_args()
+    main(args)
